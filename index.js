cbda2c746682252c1edd0049179283b4be1ccfe1
kacperh2574 <hanuszewicz.kacper@gmail.com>
2022-10-22 01:48:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-    res.status(200).json({message: 'Homepage', app: 'Escape room booking'});
+    res
+    .status(200)
+    .json({message: 'Homepage', app: 'Escape room booking'});
+});
+
+app.post('/', (req, res) => {
+    res.send('POST request');
 });
 
 app.listen(port, () => {
