f39f709878718295727e6eb22b0c73547238f436
kacperh2574 <hanuszewicz.kacper@gmail.com>
2022-10-22 19:26:52 +0200

    Start of creating API

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
+const { json } = require('express');
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 5000;
 
-app.get('/', (req, res) => {
-    res
-    .status(200)
-    .json({message: 'Homepage', app: 'Escape room booking'});
-});
+app.use(express.json());
+
+// app.get('/', (req, res) => {
+//     res
+//     .status(200)
+//     .json({message: 'Homepage', app: 'Escape room booking'});
+// });
+
+// app.post('/', (req, res) => {
+//     res.send('POST request');
+// });
+
+const rooms = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/rooms.json`)
+);
 
 app.get('/api/v1/rooms', (req, res) => {
-    
-})
+    res.status(200).json({
+        status: 'success',
+        results: rooms.length,
+        data: {
+            rooms
+        }
+    });
+});
 
-app.post('/', (req, res) => {
-    res.send('POST request');
+app.post('/api/v1/rooms', (req, res) => {
+    const newId = rooms[rooms.length - 1].id + 1;
+    const newRoom = Object.assign({ id: newId }, req.body);
+
+    rooms.push(newRoom);
+    fs.writeFile(
+        `${__dirname}/dev-data/rooms.json`,
+        JSON.stringify(rooms),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    newRoom
+                }
+            });
+        }
+    );
 });
 
 app.listen(port, () => {
