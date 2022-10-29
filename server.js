cebb3489987c3ea8883054fab3e3070bea844f74
kacperh2574 <hanuszewicz.kacper@gmail.com>
2022-10-29 16:47:08 +0200

    Test of creating a document

--- server.js
+++ server.js
@@ -5,14 +5,42 @@ const app = require('./app');
 // load environment variables
 dotenv.config();
 
-// connect to the database
 const DB = process.env.DATABASE.replace('<PASSWD>', process.env.DATABASE_PASSWD);
+
 mongoose.connect(DB, {
   useNewUrlParser: true
-}).
-then(() => console.log('Database connected...'));
+}).then(() => console.log('Database connected...'));
 
-// start the server
+const roomSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A room must have a name'],
+    unique: true
+  },
+  ratingsAvg: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A room must hava a price']
+  }
+});
+const Room = mongoose.model('Room', roomSchema);
+
+const testRoom = new Room({
+  name: "The ancient legacy",
+  ratingsAvg: 4.2,
+  price: 150
+});
+
+testRoom.save().then(doc => {
+  console.log(doc);
+}).catch(err => {
+  console.log('Error: ', err);
+});
+
+// start server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
