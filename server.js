83ec4a4389c58c8af0a65a1b6ef2d184beaab647
kacperh2574 <hanuszewicz.kacper@gmail.com>
2022-10-29 16:07:38 +0200

    Connected to the MongoDB Atlas Database

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 // load environment variables
 dotenv.config();
 
-// start server
+// connect to the database
+const DB = process.env.DATABASE.replace('<PASSWD>', process.env.DATABASE_PASSWD);
+mongoose.connect(DB, {
+  useNewUrlParser: true
+}).
+then(() => console.log('Database connected...'));
+
+// start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}...`);
